0739a487263b6d0719706f90b5849e40fd4946ce
Héléna A. Gaspar <hagax8@gmail.com>
2018-11-21 11:21:24 +0000

    Initial network architecture with example files

---
+++ CNN_graph_embedding.py
@@ -0,0 +1,55 @@
+# Copyright 2018 H. Gaspar hagax8@gmail.com
+import numpy as np
+import keras
+import keras.backend as K
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import MaxPooling2D
+from keras.models import Model
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from keras.utils import to_categorical
+from sklearn.model_selection import StratifiedKFold
+
+
+def FloatOrZero(value):
+    try:
+        return int(value)
+    except:
+        return 0
+
+
+def ProteinModel(input_shape):
+    X_input = Input(input_shape)
+    X = ZeroPadding2D((3, 3))(X_input)
+    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = BatchNormalization(axis=3, name='bn0')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+    model = Model(inputs=X_input, outputs=X, name='ProteinModel')
+    return model
+
+
+def proteinCNN(X, Y, output):
+    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
+    cvscores = []
+    for train, test in kfold.split(X, Y):
+        proteinModel = ProteinModel(X.shape[1:])
+        proteinModel.compile(loss="binary_crossentropy",
+                             optimizer="Adam", metrics=["accuracy"])
+        proteinModel.fit(x=X[train], y=Y[train],
+                         epochs=10, batch_size=20, verbose=0)
+        preds = proteinModel.evaluate(x=X[test], y=Y[test], verbose=0)
+        print("%s: %.2f%%" % (proteinModel.metrics_names[1], preds[1]*100))
+        cvscores.append(preds[1] * 100)
+    print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+    #prediction = proteinModel.predict(X[test])
+    #y_classes = np.round(prediction).astype(int)
+    #np.savetxt(output+"_predictions.txt", prediction)
+    #np.savetxt(output+"_classes.txt", y_classes)
+    # print()
+    #print ("Loss = " + str(preds[0]))
+    #print ("Test Accuracy = " + str(preds[1]))
+    plot_model(proteinModel, to_file=output+'model.png')
---
+++ Graph2Image_CV.py
@@ -0,0 +1,178 @@
+# Node2vec functions from node2vec project
+# Embedding + DL architecture: H. Gaspar 2018 hagax8@gmail.com
+
+from ugtm import eGTM
+from ugtm import landscape
+import networkx as nx
+import node2vec
+from gensim.models import Word2Vec
+import numpy as np
+from CNN_graph_embedding import proteinCNN
+import argparse
+
+def parse_args():
+    '''
+    Parses the node2vec arguments.
+    '''
+    parser = argparse.ArgumentParser(description="Run Graph-GTM CNN.")
+
+    parser.add_argument('--input', nargs='?', default='list_train',
+                        help='Graph list')
+
+    parser.add_argument('--output', nargs='?', default='output',
+                        help='Output path')
+
+    parser.add_argument('--labels', nargs='?', default='random_labels',
+                        help='Output path')
+
+    parser.add_argument('--colors', nargs='?', default='example_colors',
+                        help='Output path')
+
+    parser.add_argument('--grid_size', type=int, default=8,
+                        help='Grid size for CNN input. Default is 8 (8x8 image).')
+
+    return parser.parse_args()
+
+def read_graph(inputgraph, weighted, directed):
+    '''
+    Reads edge list and create networkx graph
+    '''
+    if weighted:
+        G = nx.read_edgelist(inputgraph,
+                             nodetype=str, data=(('weight', float),),
+                             create_using=nx.DiGraph())
+    else:
+        G = nx.read_edgelist(inputgraph,
+                             nodetype=str,
+                             create_using=nx.DiGraph())
+        for edge in G.edges():
+            G[edge[0]][edge[1]]['weight'] = 1
+    if not directed:
+        G = G.to_undirected()
+    return G
+
+
+def learn_embeddings(walks, dimensions=128, window_size=5, workers=8, iter=5):
+    '''
+    Learns embeddings using node2vec random walk.
+    '''
+    walks = [map(str, walk) for walk in walks]
+    model = Word2Vec(walks, size=dimensions,
+                     window=window_size,
+                     min_count=0, sg=1, workers=workers, iter=iter)
+    model.wv.save_word2vec_format('test')
+#    print(embedding_matrix.shape)
+    embedding_matrix = model.wv.syn0
+    words = model.wv.index2word
+    # print(words_colors)
+    # print(words)
+    return words, embedding_matrix
+
+
+def doNode2Vec(inputgraph, weighted=False, directed=False,
+               dimensions=128, window_size=5,
+               workers=8, num_walks=10,
+               walk_length=80, iter=5, p=1, q=1):
+    '''
+    node2vec pipeline for representational learning for all nodes in a graph.
+    '''
+    nx_G = read_graph(inputgraph, weighted, directed)
+    G = node2vec.Graph(nx_G, directed, p, q)
+    G.preprocess_transition_probs()
+    walks = G.simulate_walks(num_walks, walk_length)
+    words, embeddings = learn_embeddings(walks)
+    return words, embeddings
+
+
+def runTransform(embeddings, k=8, m=4, regul=0.1, s=0.3):
+    '''
+    Runs GTM dimensionality reduction.
+    '''
+    gtmodel = eGTM(k=k, m=m, regul=regul, s=s).fit(embeddings).optimizedModel
+    return gtmodel
+
+
+def graphEmbedding(inputgraph, weighted=False, directed=False,
+                   dimensions=128, window_size=5,
+                   workers=8, num_walks=10,
+                   walk_length=80, iter=5, p=1, q=1,
+                   k=8, m=4, regul=0.1, s=0.3):
+    '''
+    Runs node2vec and GTM dimensionality reduction.
+    '''
+    words, embeddings = doNode2Vec(inputgraph, weighted=weighted,
+                                   directed=directed,
+                                   dimensions=dimensions,
+                                   window_size=window_size,
+                                   workers=workers, num_walks=num_walks,
+                                   walk_length=walk_length,
+                                   iter=iter, p=p, q=q)
+    transformed = runTransform(embeddings, k=k, m=m, regul=regul, s=s)
+    return words, transformed
+
+
+def colorGraph(transformed, words, colors):
+    '''
+    Creates coloured graph from GTM dimensionality reduction output.
+    '''
+    def checkcolor(x,words,colors):
+        if x in colors:
+            return colors[x]
+        else:
+            return 0.0
+    words_colors = np.array([checkcolor(word,words,colors) for word in words]).astype(float)
+    colored_graph = landscape(transformed, words_colors)
+    return words_colors, colored_graph
+
+
+def getImage(graph, k):
+    '''
+    Creates 2D matrix from meshgrid labels (graph) and meshgrid size (k)
+    '''
+    vec = []
+    j = k
+    for i in range(k):
+        vec.append([graph[(x*j)-i-1] for x in range(1, k+1)])
+    return(vec)
+
+
+def mainworkflow(graph_list, color_file, labels, directed=True, weighted=True, output="out", k=8):
+    '''
+    Creation of embedding and network training
+    '''
+    with open(color_file, 'r') as file:
+        inline = [line.strip().split(" ") for line in file]
+    colors = {k: v for k, v in inline}
+
+    count = 0
+    with open(graph_list, 'r') as file:
+        xnametrain = [line.strip() for line in file]
+    for inputgraph in xnametrain:
+        try:
+            words, transformed = graphEmbedding(inputgraph,
+                                                directed=directed,
+                                                weighted=weighted)
+            density = getImage(np.sum(transformed.matR, axis=0), k)
+            print(density)
+            words_colors, colorgraph = colorGraph(transformed, words, colors)
+            #transformed.plot_html(output="foo", labels=words_colors,ids=words)
+            colorgraph = getImage(colorgraph, k)
+            stacked = np.dstack((density, colorgraph))
+            stacked = np.expand_dims(stacked, 0)
+            if count == 0:
+                X = stacked
+            else:
+                X = np.concatenate((X, stacked), axis=0)
+            count += 1
+        except:
+            print("Error. Not processing "+str(inputgraph)+".")
+    Y = np.random.choice([0, 1], size=X.shape[0])
+    proteinCNN(X, Y, output)
+
+
+args = parse_args()
+mainworkflow(graph_list=args.input,
+             output=args.output,
+             k=args.grid_size,
+             color_file=args.colors,
+             labels=args.labels)
